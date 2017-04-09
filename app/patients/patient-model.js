6bdcb18b56fa6f30fbecb95a3c4fe31221a53d1c
Barnaby Mercer <barnaby@mesomorphic.co.uk>
2017-04-09 23:44:38 +0000

    New data

--- app/patients/patient-model.js
+++ app/patients/patient-model.js
@@ -3,7 +3,8 @@ function findAll (callback) {
     callback(null, [
       {id: 1, name: 'Boring Dave'},
       {id: 2, name: 'Tedious Trevor'},
-      {id: 3, name: 'Yawning Jan'}
+      {id: 3, name: 'Yawning Jan'},
+      {id: 3, name: 'Beige Bernard'}
     ])
   })
 }
