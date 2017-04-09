fec9ffd78988d3d79a9df358223d29a15f2b8846
Barnaby Mercer <barnaby@mesomorphic.co.uk>
2017-04-09 23:14:04 +0000

    Changes to provoke build

--- app/patients/patient-model.js
+++ app/patients/patient-model.js
@@ -2,7 +2,8 @@ function findAll (callback) {
   setImmediate(function () {
     callback(null, [
       {id: 1, name: 'Boring Dave'},
-      {id: 2, name: 'Tedious Trevor'}
+      {id: 2, name: 'Tedious Trevor'},
+      {id: 3, name: 'Yawning Jan'}
     ])
   })
 }
