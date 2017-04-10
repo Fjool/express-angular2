ded25320b4347023e7ca667e4b1d4f3779a8ba3c
Fjool <barnaby@mesomorphic.co.uk>
2017-04-10 13:15:52 +0100

    SPDEN-11 Serve dummy index.html

--- app/index.tape.js
+++ app/index.tape.js
@@ -2,6 +2,8 @@ var app = require('./test-app')
 var test = require('tape')
 var log = require('bole')('index.tape')
 
+// TODO use tape-inspect instead; keeps the tests similar to other projects
+
 // what's the very first thing to test...
 test("make sure maths isn\'t broken", function(assert) {
     assert.equal(1, 1)
@@ -11,4 +13,12 @@ test("make sure maths isn\'t broken", function(assert) {
 
 test('the app should 404 properly', function (assert) {
   app.get('/this-path-not-found').expect(404).end(assert.end)
+})
+
+test('the app should return index.html from /public', function(assert)
+{
+  app.get('/')
+     .expect(200)
+     .expect('Content-Type', "text/html; charset=UTF-8")
+     .end(assert.end)
 })
